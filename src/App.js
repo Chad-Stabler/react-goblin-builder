a18bee7ae5b97247905547cdb3cbdb3d57e83bb3
Chad Stabler <stablerpsn@gmail.com>
2022-06-15 17:20:34 -0500

    finished handleFilter

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import GoblinForm from './GoblinForm';
 import GoblinList from './GoblinList';
 import Goblin from './Goblin';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [allGoblins, setAllGoblins] = useState([]);
+  const [allGoblins, setAllGoblins] = useState([{ name: 'Terry', HP: '3', color: 'lightgreen' }]);
   const [visibleGoblins, setVisibleGoblins] = useState([allGoblins]);
   const [goblinFormName, setGoblinFormName] = useState('');
   const [goblinFormHP, setGoblinFormHP] = useState(0);
-  const [goblinFormColor, setGoblinFormColor] = useState('blue');
+  const [goblinFormColor, setGoblinFormColor] = useState('lightgreen');
+  const [string, setString] = useState('');
 
   /* 
     track: 
@@ -27,11 +28,16 @@ function App() {
     allGoblins.push(goblin);
     // update the allGoblins array. Add the new goblin to the allGoblins array immutably.
     setAllGoblins(allGoblins.slice());
-    setGoblinFormColor('');
+    setGoblinFormColor('lightgreen');
     setGoblinFormHP(0);
     setGoblinFormName('');
     // clear out the goblin form state items by setting them to empty strings. This will cause the form to reset in the UI.
   }
+
+  useEffect(() => {
+    setVisibleGoblins(allGoblins);
+    setString('');
+  }, [allGoblins]);
 
   function handleDeleteGoblin(name) {
     const index = allGoblins.findIndex(goblin => goblin.name === name);
@@ -42,9 +48,11 @@ function App() {
     // update the allGoblins array immutably to this new, smaller array
   }
 
-  function handleFilterGoblins(search) {
+  function handleFilterGoblins(string) {
+    setString(string);
     // use the filter method to get an array of goblins whose name includes this search argument
-
+    const filteredGoblins = (allGoblins.filter(goblin => goblin.name.toLowerCase().includes(string.toLowerCase())));
+    string ? setVisibleGoblins(filteredGoblins) : setVisibleGoblins(allGoblins);
     // if there is a search argument, set the visible goblins to the filtered goblins
     // if the search argument is undefined, set the visible goblins in state to just be the array of all goblins
   }
@@ -63,7 +71,7 @@ function App() {
       <div className='goblin-filter quarter'>
         Filter Goblins
         {/* note that handleFilterGoblins is defined upstairs. This is where the allGoblins array gets filtered */}
-        <input onChange={(e) => handleFilterGoblins(e.target.value)} />
+        <input value={string} onChange={e => handleFilterGoblins(e.target.value)} />
       </div>
       <GoblinForm 
         submitGoblin={submitGoblin} 
@@ -75,7 +83,7 @@ function App() {
         setGoblinFormHP={setGoblinFormHP}
       />
       <GoblinList 
-        goblins={allGoblins} // this takes in an array of goblins. If the filteredGoblins has a length, use that array. Otherwise, use the allGoblins array 
+        goblins={visibleGoblins} // this takes in an array of goblins. If the filteredGoblins has a length, use that array. Otherwise, use the allGoblins array 
         handleDeleteGoblin={handleDeleteGoblin} // note that the goblin list has access to the ability to delete
       />
     </div>
